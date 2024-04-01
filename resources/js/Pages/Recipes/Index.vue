a23c1cd7c2207f6a3a69545837b5210b3b18523f
Gonyf <qzw@live.dk>
2024-04-01 10:34:26 +0200

    first commit

---
+++ resources/js/Pages/Recipes/Index.vue
@@ -0,0 +1,89 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+</script>
+
+<template>
+    <AppLayout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                Recipes
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="flex flex-wrap justify-center content-start gap-4 text-black dark:text-white">
+                    <div class="bg-white dark:bg-gray-800 shadow-xl sm:rounded-lg min-w-[150px] min-h-[100px]">
+                        massaman curry
+                    </div>
+                    <div class="bg-white dark:bg-gray-800 shadow-xl overflow-hidden sm:rounded-lg min-w-[150px] min-h-[100px]">
+                        <div class="p-6 text-center">
+                            fiskefilet
+                        </div>
+                        <img class="overflow-hidden" src="https://picsum.photos/200" alt="test">
+                    </div>
+                    <div class="bg-white dark:bg-gray-800 shadow-xl sm:rounded-lg min-w-[150px] min-h-[100px]">
+                        <div>
+                            fiskefilet
+                        </div>
+                        <img src="https://picsum.photos/200" alt="test">
+                    </div>
+                    <div class="bg-white dark:bg-gray-800 shadow-xl sm:rounded-lg min-w-[150px] min-h-[100px]">
+                        <div>
+                            fiskefilet
+                        </div>
+                        <img src="https://picsum.photos/200" alt="test">
+                    </div>
+                    <div class="bg-white dark:bg-gray-800 shadow-xl sm:rounded-lg min-w-[150px] min-h-[100px]">
+                        <div>
+                            fiskefilet
+                        </div>
+                        <img src="https://picsum.photos/200" alt="test">
+                    </div>
+                    <div class="bg-white dark:bg-gray-800 shadow-xl sm:rounded-lg min-w-[150px] min-h-[100px]">
+                        <div>
+                            fiskefilet
+                        </div>
+                        <img src="https://picsum.photos/200" alt="test">
+                    </div>
+                    <div class="bg-white dark:bg-gray-800 shadow-xl sm:rounded-lg min-w-[150px] min-h-[100px]">
+                        <div>
+                            fiskefilet
+                        </div>
+                        <img src="https://picsum.photos/200" alt="test">
+                    </div>
+                    <div class="bg-white dark:bg-gray-800 shadow-xl sm:rounded-lg min-w-[150px] min-h-[100px]">
+                        <div>
+                            fiskefilet
+                        </div>
+                        <img src="https://picsum.photos/200" alt="test">
+                    </div>
+                    <div class="bg-white dark:bg-gray-800 shadow-xl sm:rounded-lg min-w-[150px] min-h-[100px]">
+                        <div>
+                            fiskefilet
+                        </div>
+                        <img src="https://picsum.photos/200" alt="test">
+                    </div>
+                    <div class="bg-white dark:bg-gray-800 shadow-xl sm:rounded-lg min-w-[150px] min-h-[100px]">
+                        <div>
+                            fiskefilet
+                        </div>
+                        <img src="https://picsum.photos/200" alt="test">
+                    </div>
+                    <div class="bg-white dark:bg-gray-800 shadow-xl sm:rounded-lg min-w-[150px] min-h-[100px]">
+                        <div>
+                            fiskefilet
+                        </div>
+                        <img src="https://picsum.photos/200" alt="test">
+                    </div>
+                    <div class="bg-white dark:bg-gray-800 shadow-xl sm:rounded-lg min-w-[150px] min-h-[100px]">
+                        <div>
+                            fiskefilet
+                        </div>
+                        <img src="https://picsum.photos/200" alt="test">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
